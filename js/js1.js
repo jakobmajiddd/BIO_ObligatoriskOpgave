e2134bad871901befc9bfa2301ee4dab910217ef
martinlaursen97 <martin_laursen21@hotmail.com>
2022-03-14 21:08:08 +0100

    login form

--- js/js1.js
+++ js/js1.js
@@ -1,9 +1,51 @@
 //function to check userid & password
-function check(form) {
-    //checkes if username and password are matching
-    if(form.username.value == "Jonathan" && form.password.value == "1234") {
-    window.open('index.html')//opens the target page while Id & password matches
-  } else {
-    alert("Error Password or Username");
-    }
+//function check(form) {
+//    //checkes if username and password are matching
+//    if(form.username.value == "Jonathan" && form.password.value == "1234") {
+//    window.open('index.html')//opens the target page while Id & password matches
+//  } else {
+//    alert("Error Password or Username");
+//    }
+//  }
+//
+//const out = function (str) {
+//  console.log(str);
+//}
+
+document.addEventListener("DOMContentLoaded", createFormEventListener);
+
+function createFormEventListener() {
+  let form = document.getElementById("loginForm");
+  form.addEventListener("submit", handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formEvent = event.currentTarget;
+  const url = formEvent.action;
+
+  try {
+    const formData = new FormData(formEvent);
+    await postFormDataAsJson(url, formData);
+  } catch (err) {
+    alert(err.message);
   }
+}
+
+async function postFormDataAsJson(url, formData) {
+  const plainFormData = Object.fromEntries(formData.entries());
+  const formDataJsonString = JSON.stringify(plainFormData);
+
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: formDataJsonString
+  };
+
+  return await fetch(url, fetchOptions);
+}
+
+

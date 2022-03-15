d8f8a93707ff7c39d584c8519fd6d6ba764df9a7
martinlaursen97 <martin_laursen21@hotmail.com>
2022-03-15 19:12:06 +0100

    create movie

---
+++ js/js2.js
@@ -0,0 +1,112 @@
+const overlay = document.querySelector("#overlay");
+
+document.querySelector(".close").addEventListener("click", closeModal);
+
+let modalTitle= document.querySelector(".modal-title");
+let modalInputField = document.querySelector(".modal-input-field");
+let inputCount = 0;
+
+let form = document.querySelector(".modal-input-field");
+
+
+
+function createShow() {
+  setTitle("Create show");
+  setFormDestination("http://localhost:8080/api/shows/create", "post")
+  createInput("Movie name", "Batman...", "movieName");
+
+  openModal();
+}
+
+function setTitle(title) {
+  modalTitle.textContent = title;
+}
+
+function setFormDestination(action, method) {
+  form.setAttribute("action", action);
+  form.setAttribute("method", method);
+}
+
+function createInput(inputName, placeHolder, idName) {
+  inputCount++;
+
+  const title = document.createElement("p");
+  const text = document.createTextNode(inputName);
+  title.appendChild(text);
+
+  const input = document.createElement("input");
+  input.id = idName;
+  input.name = idName;
+  input.type = "text";
+  input.placeholder = placeHolder;
+  input.classList.add("js-input");
+
+  form.appendChild(title);
+  form.appendChild(input);
+}
+
+function openModal() {
+  overlay.style.display = "block";
+}
+
+function closeModal() {
+  overlay.style.display = "none";
+  clearModal();
+}
+
+function clearModal() {
+  modalTitle.textContent = "";
+  inputCount = 0;
+  form.reset();
+
+  while (modalInputField.hasChildNodes()) {
+    modalInputField.removeChild(modalInputField.firstChild);
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", createFormEventListener);
+
+function createFormEventListener() {
+  form.addEventListener("submit", handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formEvent = event.currentTarget;
+  const url = formEvent.action;
+
+  try {
+    const formData = new FormData(formEvent);
+    await postFormDataAsJson(url, formData);
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+async function postFormDataAsJson(url, formData) {
+  const plainFormData = Object.fromEntries(formData.entries());
+  const formDataJsonString = JSON.stringify(plainFormData);
+
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: formDataJsonString
+  };
+
+  const response = await fetch(url, fetchOptions);
+
+  if (!response) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+
+  return response.json();
+}
+
+
+
